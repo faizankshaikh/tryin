721b4a33899973ecd0abededd99140aa1750b029
faizankshaikh <faizankshaikh@gmail.com>
2015-10-15 20:40:48 +0530

    correcting error

--- Training_Dataset/random_forest_model.py
+++ Training_Dataset/random_forest_model.py
@@ -1,9 +1,11 @@
 __author__ = 'jalFaizy, ashishmokalkar79, mybestthings'
-import csv
 
+import csv
 import sys
 import os
 import pandas as pd
+import sklearn as sk
+from sklearn.metrics import accuracy_score
 
 """
 print "FILES"
@@ -12,48 +14,48 @@ print second_csv_file;
 print third_csv_file;
 print fourth_csv_file;
 
-print "FILE: ID_AGE_TRAIN "
-with open('id_age_train.csv', 'rb') as f:
+print "FILE: ID_AGE_test "
+with open('id_age_test.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         print row
 
-print "FILE: ID_LABEL_TRAIN "
+print "FILE: ID_LABEL_test "
 
-with open('id_label_train.csv', 'rb') as f:
+with open('id_label_test.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         print row
-print "FILE: ID_TIME_LABS_TRAIN "
+print "FILE: ID_TIME_LABS_test "
 
-with open('id_time_labs_train.csv', 'rb') as f:
+with open('id_time_labs_test.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         print row
-print "FILES: ID_TIME_VITALS_TRAIN"
+print "FILES: ID_TIME_VITALS_test"
 
-with open('id_time_vitals_train.csv', 'rb') as f:
+with open('id_time_vitals_test.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         print row
 """
 
-def read_args(vitals_train, labs_train, age_train):
+def read_args(vitals_test, labs_test, age_test):
     """ 
     This function reads arguments and returns pandas.DataFrame object
     """
-    train_vitals = pd.read_csv(vitals_train)
-    train_labs = pd.read_csv(labs_train)
-    train_age = pd.read_csv(age_train)
+    test_vitals = pd.read_csv(vitals_test)
+    test_labs = pd.read_csv(labs_test)
+    test_age = pd.read_csv(age_test)
     
     """
     # testing 
-    print train_age.describe()
+    print test_age.describe()
     """
     
-    return train_vitals, train_labs, train_age
+    return test_vitals, test_labs, test_age
 
-def preprocess(train_vitals, train_labs, train_age):
+def preprocess(test_vitals, test_labs, test_age):
     """
     This function preprocess data 
     1. equalize
@@ -63,16 +65,16 @@ def preprocess(train_vitals, train_labs, train_age):
     
     """
     # testing 
-    print train_age.describe()
+    print test_age.describe()
     """
     
     """
     /*
-train_labs_equi = pd.DataFrame(train_labs[train_labs.ID == 1].mean())
+test_labs_equi = pd.DataFrame(test_labs[test_labs.ID == 1].mean())
 for i in range(2, 3594):
-    train_labs_equi = pd.concat([train_labs_equi, train_labs[train_labs.ID == i].mean()], axis = 1)
+    test_labs_equi = pd.concat([test_labs_equi, test_labs[test_labs.ID == i].mean()], axis = 1)
     
-train_labs.equi = train_labs.equi.T
+test_labs.equi = test_labs.equi.T
 */
     """    
     return None
@@ -83,7 +85,7 @@ def main():
     third_csv_file=sys.argv[3];
     # fourth_csv_file=sys.argv[4];
     
-    train_vitals, train_labs, train_age = read_args(first_csv_file, second_csv_file, third_csv_file)
-    train_all = preprocess(train_vitals, train_labs, train_age)
+    test_vitals, test_labs, test_age = read_args(first_csv_file, second_csv_file, third_csv_file)
+    test_all = preprocess(test_vitals, test_labs, test_age)
 
 main()
